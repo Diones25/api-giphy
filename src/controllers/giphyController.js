19e59dfbfec8e0e947ceb0c51f0e72e404c8f151
Diones Pereira Alves <pereiradiones987@gmail.com>
2022-05-03 09:35:43 -0300

    atualizando os status de saida

--- src/controllers/giphyController.js
+++ src/controllers/giphyController.js
@@ -3,14 +3,18 @@ const api = require('../service/api.js');
 const getGifSearch = async (req, res) => {
     await api.get("/gifs/search").then((response) => {
         console.log(JSON.stringify(response.data));
-        return res.json(response.data);
+        return res.status(200).json(response.data);  
+    }).catch(() => {
+        return res.status(403).json({ message: "Gifs não encontrado!" });
     });
 }
 
 const getStickersSearc = async (req, res) => {
     await api.get('/stickers/search').then((response) => {
         console.log(JSON.stringify(response.data));
-        return res.json(response.data);
+        return res.status(200).json(response.data);  
+    }).catch(() => {
+        return res.status(400).json({ message: "Stickers não encontrado!" });
     });
 }
 
